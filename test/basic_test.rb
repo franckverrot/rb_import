6693f08198f489438bf054765b284a85be099e19
Franck Verrot <franck@verrot.fr>
2014-11-11 16:13:22 +0100

    * basic_test.rb: Ensure LoadError's are triggered

--- test/basic_test.rb
+++ test/basic_test.rb
@@ -6,4 +6,16 @@ class BasicTest < Minitest::Test
     f = import("./test/fixtures/foo.rb")
     assert_equal "foo bar", f.new.to_s
   end
+
+  def test_missing_file
+    error_occured = false
+
+    begin
+      import("unknown_file.rb")
+    rescue LoadError => error
+      error_occured = true
+    end
+
+    assert error_occured
+  end
 end
